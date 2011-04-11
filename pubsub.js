849061a2e8aae53a093353c6dec10120f5da3e47
Bob Bonifield <bobbonifield@gmail.com>
2011-04-10 22:00:37 -0500

    Adding priority support to pub/sub

--- pubsub.js
+++ pubsub.js
@@ -1,6 +1,7 @@
 /*	
 
 	jQuery pub/sub plugin by Peter Higgins (dante@dojotoolkit.org)
+	Priority added by Bob Bonifield (bobbonifield@gmail.com)
 
 	Loosely based on Dojo publish/subscribe API, limited in scope. Rewritten blindly.
 
@@ -14,11 +15,15 @@
 	// the topic/subscription hash
 	var cache = {};
 
-	d.publish = function(/* String */topic, /* Array? */args){
+	d.publish = function(/* String */topic, /* Integer? */priority, /* Array? */args){
 		// summary: 
-		//		Publish some data on a named topic.
+		//		Publish some data on a named topic with the option of only pushlishing
+		//		to callbacks with a specific priority
 		// topic: String
 		//		The channel to publish on
+		// priority: Integer?
+		//		The priority to publish at.  If not provided, every handler for a
+		//		given topic is executed.
 		// args: Array?
 		//		The data to publish. Each array item is converted into an ordered
 		//		arguments on the subscribed functions. 
@@ -28,16 +33,34 @@
 		//		with a function signature like: function(a,b,c){ ... }
 		//
 		//	|		$.publish("/some/topic", ["a","b","c"]);
-		cache[topic] && d.each(cache[topic], function(){
-			this.apply(d, args || []);
-		});
+		//	|		$.publish("/some/topic", 20, ["a","b","c"]);
+
+		if (cache[topic]){
+			var priority_supplied = !isNaN(priority),
+			    callbacks = [];
+			if (priority_supplied){
+				cache[topic][priority] && d.each(cache[topic][priority], function(){
+					this.apply(d, args || []);
+				});
+			} else {
+				args = priority;
+				d.each(cache[topic], function(){
+					d.each(this, function(){
+						this.apply(d, args || []);
+					});
+				});
+			}
+		}
 	};
 
-	d.subscribe = function(/* String */topic, /* Function */callback){
+	d.subscribe = function(/* String */topic, /* Integer? */priority, /* Function */callback){
 		// summary:
-		//		Register a callback on a named topic.
+		//		Register a callback on a named topic with an optional subscription priority
 		// topic: String
 		//		The channel to subscribe to
+		// priority: Integer?
+		//		The priority that the callback should use.  If not provided, the callback
+		//		will use the default priority.
 		// callback: Function
 		//		The handler event. Anytime something is $.publish'ed on a 
 		//		subscribed channel, the callback will be called with the
@@ -48,13 +71,23 @@
 		//	
 		// example:
 		//	|	$.subscribe("/some/topic", function(a, b, c){ /* handle data */ });
-		//
+		//	|	$.subscribe("/some/topic", 20, function(a, b, c){ /* handle data */ });
+
+		default_priority = d.subscribe.default_priority;
+		callback = callback || priority;
+		priority = callback ? priority || default_priority : default_priority;
 		if(!cache[topic]){
 			cache[topic] = [];
 		}
-		cache[topic].push(callback);
-		return [topic, callback]; // Array
+		if(!cache[topic][priority]){
+			cache[topic][priority] = [];
+		}
+		cache[topic][priority].push(callback);
+		return [topic, callback, priority]; // Array
 	};
+
+	// the priority that a subscription should use if one is not provided
+	d.subscribe.default_priority = 10;
 
 	d.unsubscribe = function(/* Array */handle){
 		// summary:
@@ -64,11 +97,14 @@
 		// example:
 		//	|	var handle = $.subscribe("/something", function(){});
 		//	|	$.unsubscribe(handle);
+		//	|	var handle = $.subscribe("/something", 20, function(){});
+		//	|	$.unsubscribe(handle);
 		
-		var t = handle[0];
-		cache[t] && d.each(cache[t], function(idx){
+		var t = handle[0],
+		    p = handle[2];
+		cache[t] && cache[t][p] && d.each(cache[t][p], function(idx){
 			if(this == handle[1]){
-				cache[t].splice(idx, 1);
+				cache[t][p].splice(idx, 1);
 			}
 		});
 	};
